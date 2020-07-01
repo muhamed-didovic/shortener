cdf9f3dbad7d35f0df3a9b82f860cecfd673084f
Muhamed Didovic <muhamed.didovic@gmail.com>
2020-07-01 23:06:14 +0200

    test for github actions

--- src/Shortener.php
+++ src/Shortener.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace MuhamedDidovic\Shortener;
 
@@ -21,14 +21,14 @@ class Shortener
      */
     public function toBase($value, $base = 62)
     {
-        $r = $value % $base;
+        $r      = $value % $base;
         $result = $this->base[$r];
-        $q = floor($value / $base);
+        $q      = floor($value / $base);
 
         while ($q) {
-            $r = $q % $base;
-            $q = floor($q / $base);
-            $result = $this->base[$r].$result;
+            $r      = $q % $base;
+            $q      = floor($q / $base);
+            $result = $this->base[$r] . $result;
         }
 
         return $result;
