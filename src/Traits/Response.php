d31a9a1dfa3ab319d8fbca7a263819d8370c6332
Muhamed Didovic <muhamed.didovic@gmail.com>
2020-07-03 07:19:07 +0200

    test

--- src/Traits/Response.php
+++ src/Traits/Response.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace MuhamedDidovic\Shortener\Traits;
 
@@ -18,14 +18,20 @@ trait Response
      */
     protected function linkResponse(Link $link, $merge = [])
     {
-        return response()->json([
-            'data' => array_merge([
-                'original_url'   => $link->original_url,
-                'shortened_url'  => $link->shortenedUrl(),
-                'code'           => $link->code,
-                'last_requested' => $link->last_requested,
-                'last_used'      => $link->last_used ?? null,
-            ], $merge),
-        ], 200);
+        return response()->json(
+            [
+                'data' => array_merge(
+                    [
+                        'original_url'   => $link->original_url,
+                        'shortened_url'  => $link->shortenedUrl(),
+                        'code'           => $link->code,
+                        'last_requested' => $link->last_requested,
+                        'last_used'      => $link->last_used ?? null,
+                    ],
+                    $merge
+                ),
+            ],
+            200
+        );
     }
 }
